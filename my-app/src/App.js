e1e403b362f3d2c6001f535ad17519294acef419
Morena Tirana <63554265+MorenaTirana@users.noreply.github.com>
2023-01-06 10:22:21 +0100

    added GitHub Url

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,5 +2,22 @@ import React from 'react';
 import './App.css';
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>React Weather App</h1>
+      <footer>
+        Coded by{' '}
+        <a href="www.morenatirana.com" target="_blank">
+          Morena Tirana
+        </a>{' '}
+        with{' '}
+        <a
+          href="https://github.com/MorenaTirana/react-weather-app"
+          target="_blank"
+        >
+          GitHub code source
+        </a>
+      </footer>
+    </div>
+  );
 }
