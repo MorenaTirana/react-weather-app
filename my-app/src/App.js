5fec50d2289750759310d2f2538d1b539a0341bd
Morena Tirana <63554265+MorenaTirana@users.noreply.github.com>
2023-01-06 14:07:59 +0100

    added basic styling

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Weather from './Weather';
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>React Weather App</h1>
+        <Weather />
         <footer>
           Coded by{' '}
           <a
