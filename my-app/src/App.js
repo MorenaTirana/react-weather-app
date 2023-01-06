1ea282bc030355d02c2e006f9f68770bab14a442
Morena Tirana <63554265+MorenaTirana@users.noreply.github.com>
2023-01-06 15:28:12 +0100

    Added API integration

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="New York" />
         <footer>
           Coded by{' '}
           <a
