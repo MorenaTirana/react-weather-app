bfb44dc8f0832ef5e1c291e9955d98b65a8093aa
Morena Tirana <63554265+MorenaTirana@users.noreply.github.com>
2023-01-06 12:23:47 +0100

    added bootstrap

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,20 +4,27 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <h1>React Weather App</h1>
-      <footer>
-        Coded by{' '}
-        <a href="www.morenatirana.com" target="_blank">
-          Morena Tirana
-        </a>{' '}
-        with{' '}
-        <a
-          href="https://github.com/MorenaTirana/react-weather-app"
-          target="_blank"
-        >
-          GitHub code source
-        </a>
-      </footer>
+      <div className="container">
+        <h1>React Weather App</h1>
+        <footer>
+          Coded by{' '}
+          <a
+            href="https://www.morenatirana.com"
+            rel="noreferrer"
+            target="_blank"
+          >
+            Morena Tirana
+          </a>{' '}
+          with{' '}
+          <a
+            href="https://github.com/MorenaTirana/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub code source
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
