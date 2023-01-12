8b283201ced87c8f78449ea83538806b132227b1
Morena Tirana <63554265+MorenaTirana@users.noreply.github.com>
2023-01-12 21:31:13 +0100

    kkkk

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,12 +6,12 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="New York" />
+        <Weather defaultCity="Stockholm" />
         <footer>
           Coded by{' '}
           <a
             href="https://www.morenatirana.com"
-            rel="noreferrer"
+            rel="noopener noreferrer"
             target="_blank"
           >
             Morena Tirana
@@ -20,7 +20,7 @@ export default function App() {
           <a
             href="https://github.com/MorenaTirana/react-weather-app"
             target="_blank"
-            rel="noreferrer"
+            rel="noopener noreferrer"
           >
             GitHub code source
           </a>
