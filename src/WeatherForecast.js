87f65f9c8a168cfeb4382d67aad0e7a6d40dc4a7
Morena Tirana <63554265+MorenaTirana@users.noreply.github.com>
2023-01-13 02:01:38 +0100

    lll

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -30,7 +30,7 @@ export default function WeatherForecast(props) {
       <div className="WeatherForecast">
         <div className="row">
           {forecast.map(function (dailyForecast, index) {
-            if (index < 6) {
+            if (index < 5) {
               return (
                 <div className="col" key={index}>
                   <WeatherForecastDay data={dailyForecast} />
